499f249bff76d5e7fe28d43198892c503c17f0da
James Gibson <james.the.nodester@gmail.com>
2020-02-01 22:09:51 -0700

    Init nx workspace

--- apps/core/src/app/app.controller.ts
+++ apps/core/src/app/app.controller.ts
@@ -1,13 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { IntakeService } from '@handled/intake'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,private readonly intakeService: IntakeService) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Get("/darksky")
+  getPostgres() {
+    return this.intakeService.triggerWeatherCollection();
+  }
+
+  //  This name is shit
+  @Get("/intake")
+  getIntake() {
+    return this.intakeService.triggerWeatherCollection();
   }
 }
--- apps/core/src/app/app.module.ts
+++ apps/core/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-//import * as Postgres from '@handled/postgres';
+import { DarkskyService } from "@handled/darksky";
+import { ConfigService } from "@handled/config";
+import { IntakeService } from "@handled/intake";
+import { PostgresService } from '@handled/postgres';
+import * as observation from '@handled/observation';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [PostgresService,ConfigService,IntakeService,DarkskyService, AppService],
 })
 export class AppModule {}
