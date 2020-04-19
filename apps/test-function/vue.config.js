1238195a75faf0f2fc46d9ab35a385dd91c3132e
James Gibson <james.the.nodester@gmail.com>
2020-04-19 00:23:23 -0600

    Match actual function

--- apps/test-function/vue.config.js
+++ apps/test-function/vue.config.js
@@ -3,5 +3,5 @@
 const isProduction = process.env.NODE_ENV === 'production';
 const isLocal = process.env.HANDLED_IS_LOCAL
 module.exports = {
-    publicPath: isProduction && !isLocal ? '/test-function/public' : '/public',
+    publicPath: isProduction && !isLocal ? '/handled-test/public' : '/public',
 }
