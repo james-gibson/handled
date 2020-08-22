de4973bc48582f3d8e7cc554ac5a01c3884c1827
James Gibson <james.the.nodester@gmail.com>
2020-08-22 00:28:23 -0600

    format:write

--- apps/test-function/src/app.controller.ts
+++ apps/test-function/src/app.controller.ts
@@ -1,19 +1,18 @@
 import { Controller, Get, Req, Res } from "@nestjs/common";
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 import { AppService } from "./app.service";
-
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hi')
+  @Get("hi")
   getHello(): string {
     return this.appService.getHello();
   }
 
-  @Get('*')
+  @Get("*")
   getRoot(@Req() request: Request): string {
-    return this.appService.getRoot({request, response:undefined});
+    return this.appService.getRoot({ request, response: undefined });
   }
 }
