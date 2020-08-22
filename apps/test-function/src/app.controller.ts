a2ca5e69afb147a437451addd40a6fe5764cff1a
James Gibson <james.the.nodester@gmail.com>
2020-08-22 00:23:38 -0600

    trigger

--- apps/test-function/src/app.controller.ts
+++ apps/test-function/src/app.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Req, Res } from "@nestjs/common";
 import { Request, Response } from 'express';
 import { AppService } from "./app.service";
 
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
