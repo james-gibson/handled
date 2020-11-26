9dd0666a8ef2e03f362561e84c08cf7eca2bcc08
Valerie Kraucunas <valeriekraucunas@gmail.com>
2020-11-25 17:01:43 -0700

    chore: Add and Eval pages added

---
+++ apps/lineup/src/views/Eval.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="eval-block">
+    <h2>Evaluate</h2>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Eval",
+};
+</script>
+
+<style lang="scss"></style>
